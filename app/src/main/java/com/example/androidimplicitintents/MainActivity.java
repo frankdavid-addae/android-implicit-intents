c8e6136c620711bbb15cfd5c87ab86e7a24c55bd
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-06 16:07:48 +0000

    custom dialog demo

--- app/src/main/java/com/example/androidimplicitintents/MainActivity.java
+++ app/src/main/java/com/example/androidimplicitintents/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.androidimplicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCall, btnCallFriend, btnWeb, btnMap;
+    Button btnCall, btnCallFriend, btnWeb, btnMap, btnDialog, btn_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         btnCallFriend = findViewById(R.id.btnCallFriend);
         btnWeb = findViewById(R.id.btnWeb);
         btnMap = findViewById(R.id.btnMap);
+        btnDialog = findViewById(R.id.btnDialog);
+        btn_dialog = findViewById(R.id.btn_dialog);
+
 
         btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +60,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenDialog dialog = new OpenDialog();
+                dialog.showDialog(MainActivity.this, "This is the dialog");
+            }
+        });
     }
+
 }
+
+
