49f0c56545f26c6e0832df922a003b3dfe2d81dd
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-06 09:29:15 +0000

    call action intent

--- app/src/main/java/com/example/androidimplicitintents/MainActivity.java
+++ app/src/main/java/com/example/androidimplicitintents/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.androidimplicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall, btnCallFriend, btnWeb, btnMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCall = findViewById(R.id.btnCall);
+        btnCallFriend = findViewById(R.id.btnCallFriend);
+        btnWeb = findViewById(R.id.btnWeb);
+        btnMap = findViewById(R.id.btnMap);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
+
+        btnCallFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0248899009"));
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
